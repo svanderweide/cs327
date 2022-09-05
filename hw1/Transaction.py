3fbe4820f663cdb912de635854aeb8b88a6bdbc3
Simon Van Der Weide <jadenster12@gmail.com>
2022-09-05 16:57:23 -0400

    Add docstrings to Transaction

--- hw1/Transaction.py
+++ hw1/Transaction.py
@@ -2,12 +2,28 @@ import datetime
 from decimal import ROUND_HALF_UP, Decimal
 
 class Transaction:
+    """
+    Contains information about an individual transaction
+    for a bank account
+    
+    Attributes:
+        amount (Decimal): stores the amount of the transaction
+        date (date): stores the date of the transaction as ISO date
+    """
 
     def __init__(self, amount, date):
+        """
+        Initializes class attributes
+        
+        Args:
+            amount (str): amount of transaction
+            date (str): date of transaction
+        """
         self.amount = Decimal(amount)
         self.date = datetime.date.fromisoformat(date)
     
     def __repr__(self):
+        """Creates string representation"""
         date = self.date.isoformat()
         amount = self.amount.quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
         return f"{date}, ${amount}"
