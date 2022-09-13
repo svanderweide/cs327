f6a24a8bd7e14a608763c8ce33921fbabd5f72e7
Simon Van Der Weide <jadenster12@gmail.com>
2022-09-12 20:39:00 -0400

    Fixed Transaction attribute hiding

--- hw1/Transaction.py
+++ hw1/Transaction.py
@@ -2,30 +2,27 @@ import datetime
 from decimal import ROUND_HALF_UP, Decimal
 
 class Transaction:
-    """
-    Contains information about an individual transaction
-    for a bank account
-    
-    Attributes:
-        amount (Decimal): amount of the transaction
-        date (date): date of the transaction as ISO date
-        automated (bool): automated (True) or manual (False)
-    """
+    """Represents an individual transaction"""
 
-    def __init__(self, amount: str, date: str, automated: bool) -> None:
-        """Initializes transaction attributes
-        
-        Args:
-            amount: amount of transaction
-            date: date of transaction
-            automated: automated vs. manual transaction
-        """
-        self._amount = Decimal(amount)
+    def __init__(self, amnt: str, date: str, automated=False) -> None:
+        self._amnt = Decimal(amnt)
         self._date = datetime.date.fromisoformat(date)
         self._automated = automated
     
     def __str__(self) -> str:
-        """Returns string representation of transaction"""
         date = self._date.isoformat()
-        amount = self._amount.quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
-        return f"{date}, ${amount}"
+        amnt = self._amnt.quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
+        return f"{date}, ${amnt}"
+    
+    def same_month(self, other) -> bool:
+        """Return True if transactions in same month"""
+        return self._date.month == other._date.month
+    
+    def same_day(self, other) -> bool:
+        """Return True if transaction on same day in same month"""
+        return self._date.day == other._date.day and self._same_month(other)
+
+    def _get_automated(self) -> bool:
+        return self._automated
+
+    automated = property(_get_automated)
