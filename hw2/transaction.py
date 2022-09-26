fdad16810e75be7e85976ecf17cf13d206c7b4b2
Simon Van Der Weide <jadenster12@gmail.com>
2022-09-25 23:18:10 -0400

    Update Transaction class for hw2

--- hw2/transaction.py
+++ hw2/transaction.py
@@ -4,34 +4,53 @@ transaction module
 implements Transaction class to store transaction information
 """
 
-import datetime
+from datetime import datetime
 from decimal import ROUND_HALF_UP, Decimal
 
 class Transaction:
     """Represents an individual transaction"""
 
-    def __init__(self, amnt: str, date: str, automated=False) -> None:
-        self._amnt = Decimal(amnt)
-        self._date = datetime.date.fromisoformat(date)
-        self._automated = automated
+    def __init__(self, amt, date=None, exempt=False) -> None:
+        """
+        Args:
+            amt (string): dollar amount of transaction
+            date (string, default=None): date in ISO format (YYYY-MM-DD)
+            exempt (bool, default=False): exempt from account limits
+        """
+        self._amt = Decimal(amt)
+        if date is None:
+            self._date = datetime.now().date()
+        else:
+            self._date = datetime.strptime(date, "%Y-%m-%d").date()
+        self._exempt = exempt
 
     def __str__(self) -> str:
-        date = self._date.isoformat()
-        amnt = self._amnt.quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
-        return f"{date}, ${amnt:,}"
+        return f"{self._date}, ${self._amt:,.2f}"
 
-    def same_month(self, other) -> bool:
-        """Return True if transactions in same month"""
-        return self._date.month == other._date.month
+    def is_exempt(self):
+        """Check if transaction is exempt from limits"""
+        return self._exempt
 
-    def same_day(self, other) -> bool:
-        """Return True if transaction on same day in same month"""
-        return self._date.day == other._date.day and self.same_month(other)
+    def in_same_month(self, other):
+        """Check if two transactions occur in same month"""
+        return self._date.year == other._date.year and self._date.month == other._date.month
 
-    def _get_automated(self) -> bool:
-        return self._automated
+    def in_same_day(self, other):
+        """Check if two transactions occur in the same day"""
+        return self._date.day == other._date.day and self.in_same_month(self, other)
 
-    def __lt__(self, other) -> bool:
+    def __radd__(self, other):
+        """Required for sum() with Transaction instances"""
+        return other + self._amt
+    
+    def check_balance(self, balance):
+        """Checks if the Transaction would overdraw the balance given
+        
+        Args:
+            balance (Decimal): current balance
+        """
+        return self._amt >= 0 or balance > abs(self._amt)
+
+    def __lt__(self, other):
+        """Compares Transactions by date"""
         return self._date < other._date
-
-    automated = property(_get_automated)
