5d7b244c9f1abdf4debe8b7c15d92ac06ab066e0
Simon Van Der Weide <jadenster12@gmail.com>
2022-12-12 15:27:25 -0500

    Implement basic Board structure

---
+++ proj6/santorini/board.py
@@ -0,0 +1,35 @@
+"""Santorini board module"""
+
+from .tile import SantoriniTile
+
+class SantoriniBoard:
+
+    def __init__(self, dim: tuple) -> None:
+        self._dim = dim
+        self._tiles = self._create_tiles()
+    
+    def _create_tiles(self) -> list[list[SantoriniTile]]:
+        tiles = []
+        for _ in range(self._dim[0]):
+            row = []
+            for _ in range(self._dim[1]):
+                row.append(SantoriniTile())
+            tiles.append(row)
+        return tiles
+    
+    def __str__(self) -> str:
+        
+        board = ''
+        row_division = '+--' * self._dim[1] + '+'
+
+        board += row_division
+        for row in range(self._dim[0]):
+            board += '\n'
+            board += '|'
+            for col in range(self._dim[1]):
+                board += str(self._tiles[row][col])
+                board += '|'
+            board += '\n'
+            board += row_division
+
+        return board
