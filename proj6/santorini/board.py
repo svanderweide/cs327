e42a2d7ca313b592f2c1ec4608f2fdf31b721c73
Simon Van Der Weide <jadenster12@gmail.com>
2022-12-12 16:21:04 -0500

    Add basic move and builld to Board class

--- proj6/santorini/board.py
+++ proj6/santorini/board.py
@@ -1,6 +1,8 @@
 """Santorini board module"""
 
 from .tile import SantoriniTile
+from .worker import SantoriniWorker
+from .exceptions import InvalidBuildException, InvalidMoveException
 
 class SantoriniBoard:
 
@@ -9,14 +11,43 @@ class SantoriniBoard:
         self._tiles = self._create_tiles()
     
     def _create_tiles(self) -> list[list[SantoriniTile]]:
-        tiles = []
+        
+        tiles = FixedWidthList()
         for _ in range(self._dim[0]):
-            row = []
+            row = FixedWidthList()
             for _ in range(self._dim[1]):
                 row.append(SantoriniTile())
             tiles.append(row)
         return tiles
+
+    def _get_tile(self, location: tuple) -> SantoriniTile:
+        return self._tiles[location[0]][location[1]]
     
+    def worker_move(self, worker: SantoriniWorker, direction: tuple):
+
+        src_location = worker.location
+        dst_location = tuple(i + j for i,j in zip(src_location, direction))
+
+        try:
+            dst_tile = self._get_tile(dst_location)
+        except IndexError:
+            raise InvalidMoveException()
+        else:
+            dst_tile.worker = worker
+            worker.location = dst_location
+
+    def worker_build(self, worker: SantoriniWorker, direction: tuple):
+
+        src_location = worker.location
+        dst_location = tuple(i + j for i,j in zip(src_location, direction))
+
+        try:
+            dst_tile = self._get_tile(dst_location)
+        except IndexError:
+            raise InvalidBuildException()
+        else:
+            dst_tile.build()
+
     def __str__(self) -> str:
         
         board = ''
@@ -33,3 +64,10 @@ class SantoriniBoard:
             board += row_division
 
         return board
+
+class FixedWidthList(list):
+
+    def __getitem__(self, idx):
+        if idx < 0:
+            raise IndexError(f'Expected positive index: got {idx}')
+        return super().__getitem__(idx)
--- proj6/tests/test_board.py
+++ proj6/tests/test_board.py
@@ -4,6 +4,8 @@ import unittest
 from unittest.mock import Mock, patch
 
 from santorini.board import SantoriniBoard
+from santorini.tile import SantoriniTile
+from santorini.worker import SantoriniWorker
 from santorini.exceptions import InvalidBuildException, InvalidMoveException
 
 class TestCore(unittest.TestCase):
@@ -31,3 +33,82 @@ class TestCore(unittest.TestCase):
                  '+--+--+--+--+--+')
         board = '\n'.join(board)
         self.assertEqual(str(self.board), board)
+
+class TestMove(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.board = SantoriniBoard((5, 5))
+        self.worker = SantoriniWorker('white', 'A')
+
+    def test_board_worker_move_tile_update(self):
+        tile: SantoriniTile = self.board._get_tile((1, 1))
+        self.board.worker_move(self.worker, (1, 1))
+        self.assertEqual(tile.worker, self.worker)
+
+    def test_board_worker_move_worker_update(self):
+        self.board.worker_move(self.worker, (1, 1))
+        self.assertEqual(self.worker.location, (1, 1))
+
+    def test_board_worker_move_board_str(self):
+        self.board.worker_move(self.worker, (1, 1))
+        board = ('+--+--+--+--+--+',
+                 '|0 |0 |0 |0 |0 |',
+                 '+--+--+--+--+--+',
+                 '|0 |0A|0 |0 |0 |',
+                 '+--+--+--+--+--+',
+                 '|0 |0 |0 |0 |0 |',
+                 '+--+--+--+--+--+',
+                 '|0 |0 |0 |0 |0 |',
+                 '+--+--+--+--+--+',
+                 '|0 |0 |0 |0 |0 |',
+                 '+--+--+--+--+--+')
+        board = '\n'.join(board)
+        self.assertEqual(str(self.board), board)
+
+    def test_board_worker_move_exception_negative_row(self):
+        self.board.worker_move(self.worker, (0, 0))
+        with self.assertRaises(InvalidMoveException):
+            self.board.worker_move(self.worker, (-1, 0))
+
+    def test_board_worker_move_exception_negative_col(self):
+        self.board.worker_move(self.worker, (0, 0))
+        with self.assertRaises(InvalidMoveException):
+            self.board.worker_move(self.worker, (0, -1))
+
+class TestBuild(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.board = SantoriniBoard((5, 5))
+        self.worker = SantoriniWorker('white', 'A')
+    
+    def test_board_worker_build_tile_update(self):
+        tile = self.board._get_tile((1, 1))
+        with patch.object(tile, 'build') as mock_build:
+            self.board.worker_build(self.worker, (1, 1))
+            mock_build.assert_called_once()
+
+    def test_board_worker_build_board_str(self):
+        self.board.worker_build(self.worker, (1, 1))
+        board = ('+--+--+--+--+--+',
+                 '|0 |0 |0 |0 |0 |',
+                 '+--+--+--+--+--+',
+                 '|0 |1 |0 |0 |0 |',
+                 '+--+--+--+--+--+',
+                 '|0 |0 |0 |0 |0 |',
+                 '+--+--+--+--+--+',
+                 '|0 |0 |0 |0 |0 |',
+                 '+--+--+--+--+--+',
+                 '|0 |0 |0 |0 |0 |',
+                 '+--+--+--+--+--+')
+        board = '\n'.join(board)
+        self.assertEqual(str(self.board), board)
+
+    def test_board_worker_build_exception_negative_row(self):
+        self.board.worker_move(self.worker, (0, 0))
+        with self.assertRaises(InvalidBuildException):
+            self.board.worker_build(self.worker, (-1, 0))
+
+    def test_board_worker_build_exception_negative_col(self):
+        self.board.worker_move(self.worker, (0, 0))
+        with self.assertRaises(InvalidBuildException):
+            self.board.worker_build(self.worker, (0, -1))
