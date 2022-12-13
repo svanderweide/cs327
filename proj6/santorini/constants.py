a094fe9aead8629a721efa1db2ced2bed1666a32
Simon Van Der Weide <jadenster12@gmail.com>
2022-12-12 22:34:03 -0500

    Add constants file

---
+++ proj6/santorini/constants.py
@@ -0,0 +1,12 @@
+"""Constants module for Santorini game-related contants"""
+
+DIRECTIONS = {
+    'n':  (-1, 0),
+    'ne': (-1, 1),
+    'e':  (0, 1),
+    'se': (1, 1),
+    's':  (1, 0),
+    'sw': (1, -1),
+    'w':  (0, -1),
+    'nw': (-1, -1)
+}
