ad7b3df51aa2918ae5f7f2f9b04cdeeb909acf30
Simon Van Der Weide <jadenster12@gmail.com>
2022-12-14 14:23:49 -0500

    Update CLI to state pattern

--- proj6/santorini/SantoriniCLI.py
+++ proj6/santorini/SantoriniCLI.py
@@ -5,61 +5,27 @@ Responsible for setting up, launching, and monitoring
 the state of the game for termination conditions
 """
 
-from santorini.board import SantoriniBoard
-from santorini.worker import SantoriniWorker
-from santorini.player import (SantoriniPlayerHuman, SantoriniPlayerRandom, SantoriniPlayerHeuristic)
+from santorini.state import (SantoriniStateBase, SantoriniStateInitial)
 
 class SantoriniCLI():
 
+    _state: SantoriniStateBase = None
+
     def __init__(self, **kwargs) -> None:
-        self._players = self._create_players(**kwargs)
-        self._board = SantoriniBoard(workers=self._create_workers())
+        self._args = kwargs
+        self.board = None
+        self.players = None
+        self.transition_to(SantoriniStateInitial())
 
-    def _create_players(self, **kwargs) -> list:
+    def _get_args(self) -> dict:
+        return self._args
+    
+    args = property(_get_args)
 
-        players = []
-
-        # hard-coded for 2-player CLI version
-        cols = ['white', 'blue']
-
-        # not hard-coded for 2-player CLI version
-        # but depends on hard-coded values above
-        strategies = {col: kwargs.get(col) for col in cols}
-
-        for col, strategy in strategies.items():
-            if strategy == 'human':
-                player = SantoriniPlayerHuman(col)
-            elif strategy == 'random':
-                player = SantoriniPlayerRandom(col)
-            elif strategy == 'heuristic':
-                player = SantoriniPlayerHeuristic(col)
-            players.append(player)
-
-        return players
-
-    def _create_workers(self) -> list[SantoriniWorker]:
-
-        # hard-coded for 2-player CLI version
-        workers = [
-            SantoriniWorker('white', 'A', (3, 1)),
-            SantoriniWorker('white', 'B', (1, 3)),
-            SantoriniWorker('blue', 'Y', (1, 1)),
-            SantoriniWorker('blue', 'Z', (3, 3)),
-        ]
-        
-        return workers
+    def transition_to(self, state) -> None:
+        self._state = state
+        self._state.context = self
 
     def run(self) -> None:
-
-        idx = 0
-        player_len = len(self._players)
-        turn = 1
-
         while True:
-            player = self._players[idx]
-            print(self._board)
-            print(f'Turn: {turn}, ', end='')
-            player.print_description(self._board)
-            player.take_turn(self._board)
-            idx = (idx + 1) % player_len
-            turn += 1
+            self._state.run()
---
+++ proj6/santorini/state.py
@@ -0,0 +1,150 @@
+"""
+SantoriniState module
+---------------------
+Implements the various states required for a Santorini game
+and allows extensions to new interfaces with minimal changes
+"""
+
+import sys
+from abc import ABC, abstractmethod
+
+from santorini.board import SantoriniBoard
+from santorini.worker import SantoriniWorker
+from santorini.player import (SantoriniPlayerHuman, SantoriniPlayerRandom, SantoriniPlayerHeuristic)
+
+
+class SantoriniStateBase(ABC):
+    """
+    SantoriniStateBase
+    ------------------
+    Abstract state class with abstract method for performing the state
+    """
+    def __init__(self) -> None:
+        super().__init__()
+        self._context = None
+
+    def _get_context(self):
+        return self._context
+
+    def _set_context(self, context):
+        self._context = context
+
+    context = property(_get_context, _set_context)
+
+    @abstractmethod
+    def run(self) -> bool:
+        pass
+
+
+class SantoriniStateInitial(SantoriniStateBase):
+    """
+    SantoriniStateInitial
+    ---------------------
+    Concrete state class used to set up and prepare to run the game
+    """
+
+    def _create_players(self, args):
+
+        players = []
+
+        # hard-coded for 2-player CLI version
+        templates = [ 
+            { 'col': 'white', 'template': args.get('white'), 'names': ['A', 'B'] },
+            { 'col': 'blue',  'template': args.get('blue'),  'names': ['Y', 'Z'] },
+        ]
+
+        # not fully hard-coded but depends on the values
+        # defined above in the hard-coded portion
+        for template in templates:
+            if template['template'] == 'human':
+                player = SantoriniPlayerHuman(template['col'], template['names'])
+            elif template['template'] == 'random':
+                player = SantoriniPlayerRandom(template['col'], template['names'])
+            elif template['template'] == 'heuristic':
+                player = SantoriniPlayerHeuristic(template['col'], template['names'])
+            players.append(player)
+
+        return players
+
+    def _create_workers(self) -> list[SantoriniWorker]:
+
+        # hard-coded for 2-player CLI version
+        workers = [
+            SantoriniWorker('white', 'A', (3, 1)),
+            SantoriniWorker('white', 'B', (1, 3)),
+            SantoriniWorker('blue', 'Y', (1, 1)),
+            SantoriniWorker('blue', 'Z', (3, 3)),
+        ]
+
+        return workers
+
+    def run(self) -> None:
+
+        # create board and players
+        self.context.board = SantoriniBoard(workers=self._create_workers())
+        self.context.players = self._create_players(self._context.args)
+
+        # print complete message
+        print('Initial state complete')
+
+        # transition to next state
+        self.context.transition_to(SantoriniStateTurn(0))
+
+
+class SantoriniStateTurn(SantoriniStateBase):
+
+    def __init__(self, turn: int) -> None:
+        super().__init__()
+        self._turn = turn
+        self._player = None
+
+    def _get_context(self):
+        return super()._get_context()
+
+    def _set_context(self, context):
+        super()._set_context(context)
+        self._player = self.context.players[self._turn % len(self.context.players)]
+
+    # subclass the 'context' setter to update 'player' attribute
+    context = property(_get_context, _set_context)
+
+    def run(self) -> None:
+
+        # print the current turn information
+        print(self.context.board)
+        print(f'Turn: {self._turn + 1}, ', end='')
+        self._player.print_description(self.context.board)
+
+        # check for termination
+        if self.context.board.check_termination(self._player):
+            self.context.transition_to(SantoriniStateEnd(self._turn + 1))
+            return
+
+        # player takes their turn
+        self._player.take_turn(self._context.board)
+
+        # go to next turn
+        self.context.transition_to(SantoriniStateTurn(self._turn + 1))
+
+
+class SantoriniStateEnd(SantoriniStateBase):
+
+    def __init__(self, turn: int) -> None:
+        super().__init__()
+        self._turn = turn
+        self._player = None
+
+    def _get_context(self):
+        return super()._get_context()
+
+    def _set_context(self, context):
+        super()._set_context(context)
+        self._player = self.context.players[self._turn % len(self.context.players)]
+
+    # subclass the 'context' setter to update 'player' attribute
+    context = property(_get_context, _set_context)
+
+    def run(self) -> None:
+
+        print(f'{self._player.col} has won')
+        sys.exit(0)
