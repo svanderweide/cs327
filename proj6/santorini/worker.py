5151ad76c3ab3e9ec4f4c0baeafc786fefc4cb18
Simon Van Der Weide <jadenster12@gmail.com>
2022-12-15 04:57:05 -0500

    Complete Worker module

--- proj6/santorini/worker.py
+++ proj6/santorini/worker.py
@@ -1,6 +1,18 @@
-"""Santorini worker module"""
+"""
+Worker module
+-------------
+Implements the Worker class (mostly a data class) that provides
+an object for the players to control as move around the board
+"""
 
 class SantoriniWorker:
+    """
+    SantoriniWorker
+    ---------------
+    Worker on the Santorini game board that belongs to a certain
+    player according to its color, is represented by its name, and
+    exists on a single tile on the board
+    """
 
     def __init__(self, col: str, name: str, location: tuple) -> None:
         self._col = col
@@ -16,13 +28,13 @@ class SantoriniWorker:
         return self._name
 
     name = property(_get_name)
-    
+
     def _get_location(self) -> tuple:
         return self._location
-    
+
     def _set_location(self, location: tuple) -> None:
         self._location = location
-    
+
     location = property(_get_location, _set_location)
 
     def __str__(self) -> str:
